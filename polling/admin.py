b41dfa83cf9b331bd02e49b66d627be5584769b9
Callum Axon <callumaxon@gmail.com>
2017-01-15 11:55:25 +0000

    Polling admin and models update

--- polling/admin.py
+++ polling/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PollingData, Seat
 
-# Register your models here.
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('state', 'sclass', 'party', 'seat_id')
+    ordering = ('state',)
+    
+class PollingDataAdmin(admin.ModelAdmin):
+    list_display = ('seat', 'avg_dem', 'avg_gop', 'avg_ind')
+    ordering = ('seat',)
+    
+admin.site.register(PollingData, PollingDataAdmin)
+admin.site.register(Seat, SeatAdmin)
+    
